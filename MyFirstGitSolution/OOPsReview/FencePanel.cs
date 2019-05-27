3003600207101c7d28f6f6afda1781718cf7f6fc
andymhlanga <50343712+andymhlanga@users.noreply.github.com>
2019-05-27 17:16:09 -0600

    New File

---
+++ MyFirstGitSolution/OOPsReview/FencePanel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+
+{
+    //the default access permissions is private
+    //if an outside user of this class wanted access to  the class contents
+    //then the access permissions needs to be public
+    //if this class was static the class would not hold data (Then it would have methods with algz pub static)
+    //for a class to hold data Do Not Make it static unless you want everyone to have access to that data.(Read and change the data)
+
+   public class FencePanel    
+    {
+
+       
+        //Properties.these are examples
+        //A property is associated with a single piece of data
+        //A property has two sub components Get and Set
+        //Get rerurns a value to the calling client
+        //Set sets a value to the property.
+        //Method can also use the object 
+        //Treat a property loosely like it is a var
+        //There are two ways to code a property one is Fully Implemented and the other is called Auto Implemented
+        //Auto Implemented property the property does not need a private data member to hold the actual data.
+        //The data storage will create its own data member and will be hidden 
+
+        // now an auto implpemented  property
+
+        public double Height { get; set; } 
+        public double Width { get; set; }
+
+        //Fully Implemented property
+        //Since the property will be doing some actions against the data you must supply
+        //a storage area
+        //This storage area is referred to as a private data member
+
+        private string _Style;
+
+        //_ underscore hide the variable from access from the internet
+        //Private member with underscore is more of a coding style. 
+
+
+        public string  Style
+        {
+            get
+            {
+                return _Style; 
+            }
+
+            set
+            {
+                
+                    if (String.IsNullOrEmpty(value)) //bool test
+                    {
+                        _Style = null;
+                    }
+                    else
+                    {     //value contains the incoming data of the property
+                          //the contents of the value need to be stored.
+                        _Style = value;
+                    }
+                
+            }
+        }
+
+        // ? means it can hold numeric or null
+        //a nullable numeric will contain a numebr or null 
+        //nullable numerics do not need to be implemented unless you have additional criteria to cover.
+        public double? Price { get; set; }
+
+        //Contructors
+        //Are exuted for the client caller during 
+        //the execuion of the new command
+        //if no constructors are coded then the default constructor is created 
+        //data storage within the class definition is set
+        //the system defaults string numeric  -> null or bool to false
+        //if coder creates one constructor then the coder is responsible for ll constructors
+
+        //default simulates the system initialization of your data storage type 
+        public FencePanel()
+        {
+           //optionally you may wish to have your own default values for the storage items.
+        }
+
+
+        //Greedy Constructor this has a parameter list of all data storage items defined within the class definition. 
+        public FencePanel(double width, double height, string style, double? price)
+        {
+            Width = width;
+            Height = height;
+            _Style = style;
+            Price = price;
+        }
+
+        //Behavior (aka method)
+
+        public double EstimateNumberOfPanels(double linearLength)
+        {           
+           
+            double numberOfPanels = linearLength / Width;
+
+            return numberOfPanels;
+        }
+
+
+    }  
+
+}
---
+++ MyFirstGitSolution/OOPsReview/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+
+            //Console is a reference to a system static class
+            //static classes are used by typing the class name
+            //static classes do not store variable data
+            //static classes are used by everyone
+            //methods of a static class will be placed after
+            //      the object dot operator
+            Console.WriteLine("Hello World");
+
+            //Create an instance of a non static class
+            //If you create a declarative using JUST the
+            //   class name then this is ONLY a pointer
+            //   that can hold an instance of the class
+            //   specified;
+            //Current value of aVariable is null
+           // FencePanel aVariable = null;
+
+            //if you use the new command with the class
+            //   the new command will physically create
+            //   an instance of the class
+
+            //when this statement is executed, the Default
+            //   constructor of the FencePanel class will
+            //   be called on your behave.
+            FencePanel anInstance = new FencePanel();
+            anInstance.Height = 5.8;
+            anInstance.Width = 8.0;
+            anInstance.Style = "Neighbour Friendly";
+
+            Console.WriteLine("height is: " + anInstance.Height.ToString());
+            Console.WriteLine("width is: " + anInstance.Width.ToString());
+            Console.WriteLine("style is: " + anInstance.Style);
+            Console.WriteLine("price is: " + anInstance.Price.ToString() + "\n");
+
+            //when this statement is executed, the Greedy
+            //   constructor of the FencePanel class will
+            //   be called on your behave.
+            FencePanel anGInstance = new FencePanel(4.0, 6.8, "private", 32.89);
+
+            Console.WriteLine("height is: " + anGInstance.Height.ToString());
+            Console.WriteLine("width is: " + anGInstance.Width.ToString());
+            Console.WriteLine("style is: " + anGInstance.Style);
+            Console.WriteLine("price is: " + anGInstance.Price.ToString() + "\n");
+            
+            //using instances of classes 
+
+            Console.WriteLine("\n Entering Data \n");
+            string menuItem = "0";
+
+            FenceEstimate CustomerFence = new FenceEstimate();
+            CustomerFence.Fence = anGInstance;
+
+            CustomerFence.Gates = new List<Gate>();
+            string inputValue;
+            Gate theGate = null; // create an empty taxi both techniques will work
+
+
+
+            do
+            {
+                theGate = new Gate();// taxi is being re-used
+                Console.WriteLine("Height");
+                inputValue = Console.ReadLine();
+                theGate.Height = double.Parse(inputValue);
+
+
+
+                Console.WriteLine("Width");
+                inputValue = Console.ReadLine();
+                theGate.Width = double.Parse(inputValue);
+
+                Console.WriteLine("Style");
+                theGate.Style = Console.ReadLine();
+
+
+                Console.WriteLine("Price");
+                inputValue = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(inputValue))
+                {
+                    theGate.Price = null;
+                }
+                else
+                {
+                    theGate.Price = double.Parse(inputValue);
+                }
+
+
+                //the local instance of gate needs to be saved to the Fence Estimate
+                CustomerFence.Gates.Add(theGate);
+
+
+                Console.WriteLine("Add another gate (Y / N)");
+                inputValue = Console.ReadLine();
+
+                //Ternary operator
+                menuItem = inputValue.ToUpper().Equals("Y") ? "1" : "0";
+
+
+            } while (menuItem != "0");
+
+            Console.WriteLine("Fence length");
+            inputValue = Console.ReadLine();
+            double Linearlength = double.Parse(inputValue);
+
+
+            //Using the customer fence data calculate the cost
+            double numebrOfPanels = CustomerFence.Fence.EstimateNumberOfPanels(Linearlength);
+
+            double FencePrice = Math.Ceiling(numebrOfPanels) * double.Parse(CustomerFence.Fence.Price.ToString());//+0.5 rounded and floor makes sure you go to the next highest numer of panels.
+
+            double gatecost = 0.0;
+
+            foreach (Gate item in CustomerFence.Gates)
+            {
+                gatecost = double.Parse(item.Price.ToString());
+            }
+
+
+
+            Console.WriteLine("Total Fence Extimate");
+            Console.WriteLine("Style chosen:");
+            Console.WriteLine("Number of Panels" + numebrOfPanels.ToString());
+            Console.WriteLine("Cost of Panels" + FencePrice.ToString());
+            Console.WriteLine("Number of Gates" + CustomerFence.Gates.Count.ToString());
+            Console.WriteLine("Total" + (FencePrice + gatecost).ToString());
+
+
+
+
+        } //eom
+    }//eoc
+}//eon
+
+
+
