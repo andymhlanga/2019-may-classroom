7572a2a932450eb656d1604465c4384ae8fdf9d5
andymhlanga <50343712+andymhlanga@users.noreply.github.com>
2019-06-05 16:56:02 -0600

    Menu Added

--- CSNet/WebApp/Site.Master.designer.cs
+++ CSNet/WebApp/Site.Master.designer.cs
@@ -13,6 +13,24 @@ namespace WebApp {
     public partial class SiteMaster {
         
         /// <summary>
+        /// BasicASPMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu BasicASPMenu;
+        
+        /// <summary>
+        /// SiteNav control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapDataSource SiteNav;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
