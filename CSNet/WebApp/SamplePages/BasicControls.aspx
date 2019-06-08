a4093facc21a1802b5bdb077cb733ee63fab636c
andymhlanga <50343712+andymhlanga@users.noreply.github.com>
2019-06-07 18:37:45 -0600

    Updated

---
+++ CSNet/WebApp/SamplePages/BasicControls.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Title="Basic Controls" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="BasicControls.aspx.cs" Inherits="WebApp.SamplePages.BasicControls" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Basic Form Controls</h1>
+
+        </div>
+    <table align="center" style="width: 80%">
+        <tr>
+            <td align="right" >Enter Course Number (1-4)&nbsp&nbsp;</td>
+            <td>
+                <asp:TextBox ID="TextBoxNumberChoice" runat="server"></asp:TextBox>
+&nbsp;&nbsp&nbsp;
+                <asp:Button ID="SubmitChoice" runat="server" Text="Submit Choice" OnClick="SubmitChoice_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td align="right" style="height: 20px">
+                <asp:Label ID="Label1" runat="server" Font-Bold="True" ForeColor="#009933" Text="Choose Radio Button List"></asp:Label>
+                &nbsp&nbsp;
+            </td>
+            <td style="height: 20px">
+                <asp:RadioButtonList ID="RadioButtonListChoice" runat="server" RepeatDirection="Horizontal" RepeatLayout="Flow">
+                    <asp:ListItem Value="1">Comp1008&nbsp;</asp:ListItem >
+                    <asp:ListItem Value="2">CPSC1517&nbsp;</asp:ListItem>
+                    <asp:ListItem Value="3">DMIT1508&nbsp;</asp:ListItem>
+                    <asp:ListItem Value="4">DMIT2018&nbsp;</asp:ListItem>
+                </asp:RadioButtonList>
+            </td>
+        </tr>
+        <tr>
+            <td align="right">
+                <asp:Literal ID="Literal1" runat="server"
+                    text="Choice: Checkbox"></asp:Literal> 
+                &nbsp&nbsp;
+            </td>
+            <td>
+                <asp:CheckBox ID="CheckBoxChoice" runat="server" Text=" (programming couse if checked)" 
+                        Font-Bold="true"/></td>
+        </tr>
+        <tr>
+            <td align="right">
+                <asp:Label ID="Label2" runat="server" Text="Display Label"></asp:Label>
+                &nbsp&nbsp;
+            </td>
+            <td >
+                <asp:Label ID="DisplayReadOnly" runat="server"></asp:Label></td>
+        </tr>
+        <tr>
+            <td align="right" >
+                <asp:Label ID="Label3" runat="server" Text="Course List Collection"></asp:Label>
+                &nbsp&nbsp;
+            </td>
+            <td>
+                <asp:DropDownList ID="CollectionList" runat="server"></asp:DropDownList>&nbsp;&nbsp;&nbsp;
+                <asp:LinkButton ID="LinkButtonSubmit" runat="server" OnClick="LinkButtonSubmit_Click">Submit From Collection</asp:LinkButton>
+            </td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center">&nbsp;
+            <asp:Label ID="MessageLabel" runat="server" ></asp:Label>
+                </td>
+        </tr>
+    </table>
+    
+
+
+            
+</asp:Content>
---
+++ CSNet/WebApp/SamplePages/BasicControls.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class BasicControls : System.Web.UI.Page
+    {
+
+        //if you need to have a variable "hanging around
+        //between postings then create a global static variable
+
+        public static List<DDLClass> DataCollection = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //THIS METHOD IS EXECUTED each and every time you process the web page 
+            //this great for inital form setup
+            //clearing old messages 
+
+            //a control is refenced in the code code-behind by using its ID = name
+            MessageLabel.Text = "";
+
+            //page initalaztion only once during post back
+
+            if (!Page.IsPostBack)
+            {
+                DataCollection = new List<DDLClass>();
+                //Load collection using greedy constr
+                DataCollection.Add(new DDLClass(1, "COMP108"));
+                DataCollection.Add(new DDLClass(2, "CPSC1517"));
+                DataCollection.Add(new DDLClass(4, "DMIT2018"));
+                DataCollection.Add(new DDLClass(3, "DMIT1508"));
+
+
+                //list of T can be sorted using .sort(delegate);
+                //delegate syntax is 
+                //Asceding sort
+                //for   do the following 
+                //(x,y) =>      x.PropertyName.CompareTo(y.PropertyName)
+
+                //Desceding sort
+                //for   do the following 
+                //(x,y) =>      y.PropertyName.CompareTo(x.PropertyName)
+
+                 DataCollection.Sort((x,y) => x.ValueField.CompareTo(y.ValueField));
+
+                //attatch the data to the drop down list control
+                //attach data
+                CollectionList.DataSource = DataCollection;
+                //assign the display text property to the ddl control HARD CODED
+
+                CollectionList.DataTextField = "DisplayField";
+
+                //assign the value data property to the ddl control OOP
+
+                CollectionList.DataValueField = nameof(DDLClass.ValueField);
+
+                //Bind the data to the control
+
+                CollectionList.DataBind();
+
+                //optionally add a prompt line
+
+                CollectionList.Items.Insert(0, "SELECT.....");
+
+
+            }
+        }
+
+        protected void SubmitChoice_Click(object sender, EventArgs e)
+        {
+            //Validate that a text box value was entered
+            if (string.IsNullOrEmpty(TextBoxNumberChoice.Text))
+            {
+
+                MessageLabel.Text = "Enter a course number (1-4)";
+
+            }
+            else
+            {
+
+
+                //grab the value from the text box to set the radio button list 
+                string numberchoice = TextBoxNumberChoice.Text;
+
+                //use this value to set the check box
+                //access techniques to a quote  "List" control
+                //.SelectedValue gets / sets to the value field value
+                //.SelectedIndex    get/sets to the control index value to check is choice was made 
+                //.SelectedItem.Text   get the control display value
+                RadioButtonListChoice.SelectedValue = numberchoice;
+
+                //set a check box
+                if(numberchoice.Equals("2") || numberchoice.Equals("4"))
+                {
+                    CheckBoxChoice.Checked = true;
+                }
+               
+
+
+                // use this value to position inside the ddl
+                CollectionList.SelectedValue = numberchoice;
+
+                //use the various data access techniques to obtain diff values from the ddland display as a read only text
+
+                DisplayReadOnly.Text = CollectionList.SelectedItem.Text + " at Index" + CollectionList.SelectedIndex + " has a value of " + CollectionList.SelectedValue;
+
+            }
+
+        }
+
+        protected void LinkButtonSubmit_Click(object sender, EventArgs e)
+        {
+            //enter your code
+        }
+
+    }
+}
