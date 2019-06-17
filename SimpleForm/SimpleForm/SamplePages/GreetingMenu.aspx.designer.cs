a46a280125752f6c0c75fdfaab65ea3d33129168
andymhlanga <50343712+andymhlanga@users.noreply.github.com>
2019-06-17 17:47:50 -0600

    Update GreetingMenu.aspx.designer.cs

--- SimpleForm/SimpleForm/SamplePages/GreetingMenu.aspx.designer.cs
+++ SimpleForm/SimpleForm/SamplePages/GreetingMenu.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace SimpleForm.SamplePages {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredLastName;
         
         /// <summary>
-        /// RequiredUserName control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredUserName;
-        
-        /// <summary>
         /// RegExpEmailAddress control.
         /// </summary>
         /// <remarks>
